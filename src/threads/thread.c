5a23160b7e58ad66df0d5c97a998029ad74d9460
Myeongjin Choi <achoi0330@gmail.com>
2016-03-29 03:49:00 -0700

    basic data structure

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -450,7 +450,9 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  t->base_priority = 0;
   t->magic = THREAD_MAGIC;
+  list_init(&t->lock_list);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,11 +88,12 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int base_priority;
 
     /* added for project1 */
     int64_t wake_time;
     struct lock *wait_lock;
-    struct list waiting_list;
+    struct list lock_list;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
